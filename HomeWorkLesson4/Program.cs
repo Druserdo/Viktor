d64695a810af7c9daded34cb7a4db9cbf8ea00e1
Druserdo <Druserdo@gmail.com>
2024-01-15 23:40:16 +0300

    HomeWorkL4

---
+++ HomeWorkLesson4/Program.cs
@@ -0,0 +1,6 @@
+﻿int Ms = 13015126;
+int St = 5598486;
+Console.WriteLine($"В Питере {St} жителей.");
+Console.WriteLine($"В Москве {Ms} жителей.");
+int res = Ms - St;
+Console.WriteLine($"В Москве на {res} жителей больше.");
